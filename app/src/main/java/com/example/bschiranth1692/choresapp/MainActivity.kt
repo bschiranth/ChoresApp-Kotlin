1cb6a326b080bda276d15b3be0793707fe6b027f
bschiranth <bschiranth@gmail.com>
2017-09-28 21:13:57 -0700

    first commit

---
+++ app/src/main/java/com/example/bschiranth1692/choresapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.bschiranth1692.choresapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
