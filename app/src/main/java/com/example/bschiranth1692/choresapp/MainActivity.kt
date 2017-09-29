8d4150e581cbaf55a8e30ba14aa3c58ee82bafd2
bschiranth <bschiranth@gmail.com>
2017-09-28 22:09:09 -0700

    added read chore function

--- app/src/main/java/com/example/bschiranth1692/choresapp/MainActivity.kt
+++ app/src/main/java/com/example/bschiranth1692/choresapp/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.bschiranth1692.choresapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bschiranth1692.choresapp.data.ChoresDatabaseHelper
 
 class MainActivity : AppCompatActivity() {
+
+    var dbHelper: ChoresDatabaseHelper? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        dbHelper = ChoresDatabaseHelper(this)
     }
 }
--- app/src/main/java/com/example/bschiranth1692/choresapp/data/ChoresDatabaseHelper.kt
+++ app/src/main/java/com/example/bschiranth1692/choresapp/data/ChoresDatabaseHelper.kt
@@ -2,9 +2,12 @@ package com.example.bschiranth1692.choresapp.data
 
 import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import com.example.bschiranth1692.choresapp.data.model.Chore
+import java.text.DateFormat
+import java.util.*
 
 /**
  * Created by bschiranth1692 on 9/28/17.
@@ -34,6 +37,7 @@ class ChoresDatabaseHelper(context: Context): SQLiteOpenHelper(context, DATABASE
         onCreate(db)
     }
 
+    //CRUD
     //inserts new chore
     fun createChore(chore: Chore){
         var db :SQLiteDatabase = writableDatabase
@@ -49,6 +53,35 @@ class ChoresDatabaseHelper(context: Context): SQLiteOpenHelper(context, DATABASE
         //insert into db
         db.insert(TABLE_NAME,null,values)
 
+        //close db
+        db.close()
+
+    }
+
+    //read chore
+    fun readChore(id:Int): Chore{
+        var db :SQLiteDatabase = writableDatabase
+
+        //get cursor
+        var cursor:Cursor = db.query(TABLE_NAME, arrayOf(KEY_ID, KEY_CHORE_NAME,
+                KEY_CHORE_ASSIGNED_BY, KEY_CHORE_ASSIGNED_TO, KEY_CHORE_ASSIGNED_TIME),
+                KEY_ID+"=?", arrayOf(id.toString()),null,null,null,null)
+
+        if(cursor != null){
+            cursor.moveToFirst()
+        }
+
+        var chore = Chore()
+        chore.choreName = cursor.getString(cursor.getColumnIndex(KEY_CHORE_NAME))
+        chore.assignedBy= cursor.getString(cursor.getColumnIndex(KEY_CHORE_ASSIGNED_BY))
+        chore.assigedTo= cursor.getString(cursor.getColumnIndex(KEY_CHORE_ASSIGNED_TO))
+        chore.timeAssigned= cursor.getLong(cursor.getColumnIndex(KEY_CHORE_ASSIGNED_TIME))
+
+        var dateFormat: DateFormat = DateFormat.getDateInstance()
+        //formatted time
+        var formattedDate = dateFormat.format(Date(cursor.getLong(cursor.getColumnIndex(KEY_CHORE_ASSIGNED_TIME))).time)
+
+        return chore
     }
 
 }
